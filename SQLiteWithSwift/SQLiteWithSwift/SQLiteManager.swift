ccbad1ede3951b53111b378e32e76a2372f7e9b0
Joyann <joyann.init@gmail.com>
2016-01-24 23:17:28 +0800

    Add execute sql function.

--- SQLiteWithSwift/SQLiteWithSwift/SQLiteManager.swift
+++ SQLiteWithSwift/SQLiteWithSwift/SQLiteManager.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class SQLiteManager: NSObject {
     
+    let handler: COpaquePointer = nil
+    
     private static let sharedInstance: SQLiteManager = SQLiteManager()
     
     class func sharedManager() -> SQLiteManager {
         return sharedInstance
     }
     
-    func executeSQL(statement: String) {
-        
+    func executeSQL(statement: String) -> Bool {
+        let sql = statement.cStringUsingEncoding(NSUTF8StringEncoding)!
+        return sqlite3_exec(handler, sql, nil, nil, nil) == SQLITE_OK
     }
     
 }
